ef27d9ac812adce4f7122ec6bd20c83475516280
2302Nikolay <Malzewn@gmail.com>
2022-05-22 14:59:54 +0300

    Второй пункт второго индивидуального задания

--- Индивидуальное задание/Task_2_2.py
+++ Индивидуальное задание/Task_2_2.py
@@ -10,13 +10,9 @@ if __name__ == '__main__':
         print("Заданный список пуст", file=sys.stderr)
         exit(1)
 
-    a_min = abs(a[0])
-    i_min = 0
-    for i in a:
-        if abs(i) < a_min:
-            a_min = abs(i)
-            i_min = a.index(i)
-
-    print("Минимальный по модулю элемент: ", a[i_min])
-
-
+    ind = a.index(0)
+    s = 0
+    for n, i in enumerate(a):
+        if n > ind:
+            s += i
+    print("Сумма элементов после нуля: ", s)
